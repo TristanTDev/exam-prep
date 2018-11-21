63f3f98d95b7f2347ca3edff72f0d24ea49c488f
Tristan <tristan@Tristans-MacBook-Pro.local>
2018-11-21 15:40:30 +0000

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace RegexTesting
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+    }
+
+    public Program()
+    {
+
+    }
+
+    private void WebRequests()
+    {
+
+    }
+
+    private void CreateDirectory()
+    {
+      var drives = DriveInfo.GetDrives().Where(drive => drive.Name == "/Volumes/KINGSTON");
+      if (drives.ElementAt(0) != null)
+      {
+        var info = Directory.CreateDirectory($"{drives.ElementAt(0)}/TestDir");
+
+        Console.ReadKey();
+
+        info.Delete();
+      }
+    }
+  }
+}
